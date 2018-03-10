35f6fcc4f98a4fd273aa704985f80798074ba404
technolog <viktor@allz.su>
2018-03-10 22:58:03 +0300

    mysql

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,21 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 
 gulp.task('test', function() {
-	return gulp.src(['./tests/*.js'], {read: false})
-		.pipe(
-			mocha({
-				reporter: 'spec',
-				bail: true
-			})
-		);
+    return gulp.src(['./tests/**/*.js'], {read: false})
+        .pipe(mocha({
+            reporter: 'spec',
+            bail: true
+        }));
 });
 
 gulp.task('lint', function() {
-	return gulp.src('./lib/**/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('unix'));
+    return gulp.src('./lib/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('unix'));
 });
 
 gulp.task('lintTests', function() {
-	return gulp.src('./tests/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('unix'));
+    return gulp.src('./tests/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('unix'));
 });
